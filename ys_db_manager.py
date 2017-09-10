22d2dccfa3c3c309afbb59021288e4a740fa7198
0xAlexd <30420236+0xAlexd@users.noreply.github.com>
2017-09-10 19:52:28 +0200

    v0.4
    Code cleanup, basic database & parser methods

--- ys_db_manager.py
+++ ys_db_manager.py
@@ -1,89 +1,148 @@
 # coding: utf-8
 # !/usr/bin/python3
+try:
+    import pymysql
+except Exception as e:
+    print('ERROR_IMPORTING_PYMYSQL_MODULE_{}'.format(e[1]))
+
+try:
+    import re
+except Exception as e:
+    print('ERROR_IMPORTING_RE_MODULE_{}'.format(e[1]))
+
+try:
+    import os
+except Exception as e:
+    print('ERROR_IMPORTING_OS_MODULE_{}'.format(e[1]))
 
 
-class YsDatabase:
+class YsDatabase(object):
+
+    CURSOR = None
+    HOST = None
+    USER = None
+    PASS = None
+    CONNECTION = None
+    CONNECTED = False
 
     def __init__(self):
-        print('init')
+        return
 
-    # def setChecked(cursor, website):
-    #     try:
-    #         req = "UPDATE ys2websites SET checked = 1 WHERE website = '%s'" % (website)
-    #         cursor.execute(req)
-    #         print colored.cyan('OK UPDATE ?')
-    #     except:
-    #         print colored.red('[ERROR]		Updating checked failed... Infinite loop incoming')
-    #
-    # def getUnchecked(cursor):
-    #     cursor.execute("SELECT website, checked FROM ys2websites WHERE checked = '0'")
-    #     return cursor.fetchall()
-    #
-    # def getAll(cursor):
-    #         cursor.execute("SELECT website FROM ys2websites")
-    #         return cursor.fetchall()
-    #
-    # def insertWebsite(cursor, website, extension, title, description, languageCode, checked, mail_count, date):
-    #     try:
-    #         req = "INSERT INTO ys2websites(website, title, description, languageCode, checked, mail_count, date) VALUES('%s','%s','%s','%d', '%d', '%d')" % (website, extension, title, description, languageCode, checked, mail_count, date)
-    #         cursor.execute(req)
-    #         print colored.green('[OK]		') + "New website added : %s" % (website)
-    #     except:
-    #         print colored.red('[ERROR]		') + 'Website "%s" not added' % (website)
-    #         #print 	'Website = %s\ntitle = %s\ndescription = %s\nlanguageCode = %s\nchecked = %d\nmail_count= %d\ndate= %d\n' % (website, title, description, languageCode, checked, mail_count, date)
-    #
-    #
-    # def exec_sql_file(cursor, sql_file):
-    #     statement = ''
-    #     for line in open(sql_file):
-    #         if re.match(r'--', line):
-    #             continue
-    #         if not re.search(r'[^-;]+;', line):
-    #             statement = statement + line
-    #         else:
-    #             statement = statement + line
-    #             cursor.execute(statement)
-    #             statement = ""
+    def connect(self):
+        return
 
-        while con_flag == False:
-    try:
-        con = pymysql.connect(bd_host, bd_user, bd_pass)
-        cur = con.cursor()
-        print colored.green('[OK]		') + 'Database @'+bd_host+' successfully connected.'
-        con_flag = True
-    except:
-        print colored.red('[ERROR] ') + 'Failed to connect database. (MySQL database required to launch YS.)'
-        bd_host = raw_input(colored.cyan('\rDatabase host :'))
-        bd_user = raw_input(colored.cyan('\rDatabase user :'))
-        db_pass = raw_input(colored.cyan('\rDatabase pass :'))
+    def url_exist(self):
+        return
 
-    print 'Checking Database structure...'
-    DbExistQuery = ("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'pys'")
-    DbExist = cur.execute(DbExistQuery)
 
-    while not DbExist:
-        print colored.red('[ERROR]		') + 'Database pys does not exist @'+bd_host+' for user ' + bd_user
-        if os.path.isfile('pyYellowSpider.sql'):
-            print colored.green('[OK] ') + 'pyYellowSpider.sql found. Creating database structure...'
-            try:
-                DbCreateQuery = ("CREATE DATABASE pys")
-                cur.execute(DbCreateQuery)
-                cur.close()
-                con.close()
-                con = pymysql.connect(bd_host, bd_user, bd_pass, 'pys')
-                cur = con.cursor()
-                exec_sql_file(cur, 'pyYellowSpider.sql')
-                cur.execute("INSERT INTO ys2websites VALUES(1,'http://www.lemonde.fr', '', '', '', '', 0, 0, 000000000)");
-                con.commit()
-                DbExist = True
-                print colored.green('[OK]		') +  'Database struture created !'
-                cur.close()
-                con.close()
-            except:
-                print colored.red('[ERROR]		') + 'Error creating Database Structure ! Press any key to retry.'
-                cur.execute(("DROP DATABASE pys"))
-                DbExist = False
-                raw_input()
+class YsDatabaseMysql(YsDatabase):
+
+    def __init__(self):
+        super(YsDatabaseMysql, self).__init__()
+
+    def connect(self, host, user, pasw):
+        try:
+            self.HOST = host
+            self.USER = user
+            self.PASS = pasw
+            self.CONNECTION = pymysql.connect(self.HOST, self.USER, self.PASS)
+            self.CURSOR = self.CONNECTION.cursor()
+            return True
+        except pymysql.err.OperationalError as e:
+            print('ERROR_MYSQL_CONNECTION_REFUSED_{0}@{1}_{2}'.format(self.USER, self.HOST, e[1]))
+            return False
+
+    def url_exist(self, url):
+        query = "SELECT count(*) FROM ys2websites WHERE website == '{}'".format(url)
+        res = self.CURSOR.execute(query)
+        self.CURSOR.close()
+        return True if res > 0 else False
+
+    def insert_url(self, ys_url):
+        if self.CONNECTED:
+            if not self.url_exist(ys_url.getUrl()):
+                query = "INSERT INTO ys2websites(website, " \
+                        "                        title, " \
+                        "                        description, " \
+                        "                        languageCode, " \
+                        "                        checked, " \
+                        "                        mail_count, " \
+                        "                        date)" \
+                        " VALUES('{0}', '{1}','{2}','{3}', '{4}', '{5}')"\
+                        .format(
+                                ys_url.get_url(),
+                                ys_url.get_extension(),
+                                ys_url.get_title(),
+                                ys_url.get_country(),
+                                0,
+                                ys_url.get_mail_count(),
+                                ys_url.get_date())
+                self.CURSOR.execute(query)
+                self.CURSOR.commit()
+                self.CURSOR.close()
+                return True
+            return True
         else:
-            print colored.red('[ERROR]		') + 'pyYellowSpider.sql NOT FOUND!'
-            DbExist = False
+            return False
+
+    def set_checked(self, url):
+        if self.CONNECTED:
+            query = "UPDATE ys_website SET checked = 1 WHERE url = '{0}'".format(url)
+            self.CURSOR.execute(query)
+            self.CURSOR.commit()
+            self.CURSOR.close()
+            return True
+        else:
+            return False
+
+    # A REVOIR
+    def exec_sql_file(self, sql_file):
+        statement = ''
+        for line in open(sql_file):
+            if re.match(r'--', line):
+                continue
+            if not re.search(r'[^-;]+;', line):
+                statement = statement + line
+            else:
+                statement = statement + line
+                self.CURSOR.execute(statement)
+                statement = ''
+
+    def get_unchecked_websites(self):
+        if self.CONNECTED:
+            query = "SELECT website, checked FROM ys2websites WHERE checked = '0'"
+            self.CURSOR.execute(query)
+            self.CURSOR.commit()
+            res = self.CURSOR.fetchall()
+            self.CURSOR.close()
+            return res
+        else:
+            print('ERROR_DATABASE_NOT_CONNECTED')
+            return None
+
+    def is_schema_ok(self):
+        query = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'pys'"
+        database_exist = self.CURSOR.execute(query)
+        return True if database_exist else False
+
+    def import_schema(self):
+        if self.CONNECTED:
+            if os.path.isfile('pyYellowSpider.sql'):
+                query = "CREATE DATABASE pys"
+                self.CURSOR.execute(query)
+                self.CURSOR.close()
+                self.CONNECTION = pymysql.connect(self.HOST, self.USER, self.PASS, 'pys')
+                self.CURSOR = self.CONNECTION.cursor()
+                self.exec_sql_file('pyYellowSpider.sql')
+                # A AJOUTER AU SCHEMA + DELETE
+                self.CURSOR.execute("INSERT INTO ys2websites VALUES(1,'http://www.lemonde.fr', '', '', '', '', 0, 0, 000000000)");
+                self.CONNECTION.commit()
+                self.CURSOR.close()
+                return True
+            else:
+                print('ERROR_MYSQL_YS_SCHEMA_NOT_FOUND')
+                return False
+        else:
+            print('ERROR_DATABASE_NOT_CONNECTED')
+            return False
+
--- ys_parser.py
+++ ys_parser.py
@@ -2,95 +2,122 @@
 
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-import urlopen
 import bs4 as BeautifulSoup
 import re
+from config import REGEX
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
-class YsParser:
+class YsUrl:
 
-    url = ''
-    soup = ''
-    data = ''
-
-    url_array = []
-    mails = []
-    contactPage = ''
+    URL = None
+    HTML_CODE = None
+    SOUP = None
 
     def __init__(self, url):
-        self.url = url
-        html = requests.get(self.url, verify=False).text
-        # html = urlopen(self.url).read()
-        self.soup = BeautifulSoup.BeautifulSoup(html)
-        self.data = self.soup.prettify()
 
-    # # param = 0 for all links, param = 1 for unique ones
-    # def setUrls(self, param):
-    #     if param == 0:
-    #         for link in self.soup.find_all('a'):
-    #             self.url_array.append(link.get('href'))
-    #     if param == 1:
-    #         for link in self.soup.find_all('a'):
-    #             if link not in self.url_array:
-    #                 self.url_array.append(link.get('href'))
-    #
-    # def cleanUrls(self):
-    #     # URL_REG = "(?P<url>http(s)?://[^\s]+)"
-    #     URL_REG = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-    #     for u in self.url_array:
-    #         if not re.match(URL_REG, u):
-    #             self.url_array.remove(u)
-    #
-    # # param = 0 for all links, param = 1 for unique ones
-    # def setUrlsR(self, param):
-    #     URL_REG = "(?P<url>https?://[^\s]+)"
-    #     if param == 0:
-    #         self.url_array = re.findall(URL_REG, self.data)
-    #     if param == 1:
-    #         self.url_array = set(re.findall(URL_REG, self.data))
-    #
-    # # Getters
-    # def getUrls(self):
-    #     return self.url_array
-    #
-    # # 0 for html.parsed links, 1 for regex ones
-    # def displayUrls(self, param):
-    #     if param == 0:
-    #         c_url = self.urls
-    #     if param == 1:
-    #         c_url = self.urlsReg
-    #     for link in c_url:
-    #         print link
-    #
-    # def setMails(self):
-    #     MAIL_REG = r'[\w\.-]+@[\w\.-]+'
-    #     self.mails = set(re.findall(MAIL_REG, self.data))
-    #
-    # def setContactPage(self, url_array):
-    #     for link in url_array:
-    #         test = re.search('contact', link)
-    #         if test is not None:
-    #             self.contactPage = link
-    #
-    # def getInside(self, data, borneInf, borneSup):
-    #     temp = data.split(borneInf)
-    #     temp2 = temp[1].split(borneSup)
-    #     return temp2[0]
-    #
-    # def saveData(self, fileName, data_array):
-    #     file = open(fileName, 'w')
-    #     for line in data_array:
-    #         file.write(line)
-    #         file.write('\n')
-    #         print 'writing ... ' + line
-    #     file.close()
-    #
-    #     # WEBSITE REGEX PHP
-    #     # "/(http(s?):\/\/)?(www\.)+[a-zA-Z0-9\.\-\_]+(\.[a-zA-Z]{2,3})+(\/[a-zA-Z0-9\_\-\s\.\/\?\%\#\&\=]*)?/i";
-    #     # WEBSITE REGEX PYTHON1
-    #     # http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+
-    #
-    #     # MAIL REGEX
-    #     # "/[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}/i";
+        self.URL = url
+        try:
+            html = requests.get(self.URL, verify=False).text
+            self.SOUP = BeautifulSoup.BeautifulSoup(html, 'html.parser')
+            self.HTML_CODE = self.SOUP.prettify()
+        except requests.exceptions.ConnectionError:
+            self.URL = None
+            self.HTML_CODE = None
+            self.SOUP = None
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__['URL'] == other.__dict__['URL']
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    # 
+    def is_valid(self):
+        return True if self.URL is not None else False
+
+    def get_protocol(self):
+        if self.URL is not None:
+            valid_protocol = ['HTTP', 'HTTPS', 'FTP']
+            url_protocol = self.URL.split('://')[0]
+            if url_protocol.upper() in valid_protocol:
+                return url_protocol
+            else:
+                print('ERROR_PROTOCOL_NOT_VALID')
+                return None
+        else:
+            print('ERROR_URL_NOT_VALID')
+            return None
+
+    def get_body(self):
+        return
+
+    def get_extension(self):
+        if self.URL is not None:
+            return
+
+    def get_extension_country(self):
+        return
+
+    def get_html_code(self):
+        return self.HTML_CODE
+
+    def get_cms(self):
+        return
+
+    def get_author(self):
+        return
+
+    def get_keywords(self):
+        return
+
+    def get_urls(self):
+        if self.URL is not None:
+            parsed_urls = []
+
+            # Finding urls with soup parser
+            for link in self.SOUP.find_all('a'):
+                parsed_urls.append(link.get('href'))
+
+            # Finding urls with regex
+            regex_urls = re.findall(REGEX.URL, self.HTML_CODE)
+
+            # Distinct urls
+            return list(set(parsed_urls + regex_urls))
+        else:
+            print('ERROR_URL_NOT_VALID')
+            return None
+
+    def get_emails(self):
+        if self.URL is not None:
+            return set(re.findall(REGEX.MAIL, self.HTML_CODE))
+        else:
+            print('ERROR_URL_NOT_VALID')
+
+    # TODO_____________________
+    def get_contact_page(self):
+        for link in self.get_urls():
+            test = re.search('contact', link)
+            contact_page = link if test is not None else None
+        return contact_page
+
+
+class YsUrlCollection:
+
+    URLS = []
+
+    def __init__(self):
+        pass
+
+    def add_url(self, new_url):
+        self.URLS.append(new_url)
+
+    def add_url_list(self, url_list):
+        self.URLS = list(set(self.URLS + url_list))
+
+    def count(self):
+        return len(self.URLS)
